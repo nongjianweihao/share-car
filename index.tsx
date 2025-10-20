8b7f718c828783b3e3ba62281a756f5a59b88eba
nongjianweihao <nongjianweihao@gmail.com>
2025-10-20 12:35:24 +0800

    refactor: rely on vite entry for bundles

--- index.tsx
+++ index.tsx
@@ -1,3 +1,4 @@
+import './index.css';
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import FitnessComparisonCard from './components/Sports/FitnessComparisonCard';
